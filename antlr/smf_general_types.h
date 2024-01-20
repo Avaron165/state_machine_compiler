b508909d267fe2a484be92b6e8ef5b89e16a5503
Uli Trautenberg <info@trautenberg-engineering.de>
2024-01-20 02:12:55 +0100

    removed antlr tryout directory

--- antlr/smf_general_types.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-typedef enum smf_return_e {
-    SMF_RETURN_OK = 0,
-    SMF_RETURN_STATEMACHINE_FINISHED = 1,
-    SMF_RETURN_INTERNAL_ERROR = 10,
-} smf_return_t;
--- antlr/smf_logging.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-
-void smf__log_transition(const char* str);
-void smf__log_state_function(const char* str);
-void smf__log_event(const char* str);
